d9cbc9df070c881ccda553d1cc0f00fb4728e68e
Tim Lukashevich <silvernet@yandex.ru>
2026-05-17 21:05:37 +0300

    wip

--- config/bindings.lua
+++ config/bindings.lua
@@ -185,7 +185,7 @@ local keys = {
 
    -- panes: zoom+close pane
    { key = 'Enter', mods = mod.SUPER,     action = act.TogglePaneZoomState },
-   -- { key = 'w',     mods = mod.SUPER,     action = act.CloseCurrentPane({ confirm = false }) },
+   { key = 'w',     mods = 'CTRL|SHIFT',    action = act.CloseCurrentPane({ confirm = false }) },
 
    -- panes: navigation
    -- { key = 'k',     mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Up') },
