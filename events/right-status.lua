b28327cbaaf479fcdc02d035c92523e78e458f95
Tim Lukashevich <silvernet@yandex.ru>
2026-09-11 23:08:49 +0300

    remove calendar and battery icons

--- events/right-status.lua
+++ events/right-status.lua
@@ -1,5 +1,4 @@
 local wezterm = require('wezterm')
-local umath = require('utils.math')
 local Cells = require('utils.cells')
 local OptsValidator = require('utils.opts-validator')
 
@@ -18,40 +17,9 @@ EVENT_OPTS.schema = {
 }
 EVENT_OPTS.validator = OptsValidator:new(EVENT_OPTS.schema)
 
-local nf = wezterm.nerdfonts
 local attr = Cells.attr
 
 local M = {}
-
-local ICON_SEPARATOR = nf.oct_dash
-local ICON_DATE = nf.fa_calendar
-
----@type string[]
-local discharging_icons = {
-    nf.md_battery_10,
-    nf.md_battery_20,
-    nf.md_battery_30,
-    nf.md_battery_40,
-    nf.md_battery_50,
-    nf.md_battery_60,
-    nf.md_battery_70,
-    nf.md_battery_80,
-    nf.md_battery_90,
-    nf.md_battery,
-}
----@type string[]
-local charging_icons = {
-    nf.md_battery_charging_10,
-    nf.md_battery_charging_20,
-    nf.md_battery_charging_30,
-    nf.md_battery_charging_40,
-    nf.md_battery_charging_50,
-    nf.md_battery_charging_60,
-    nf.md_battery_charging_70,
-    nf.md_battery_charging_80,
-    nf.md_battery_charging_90,
-    nf.md_battery_charging,
-}
 
 ---@type table<string, Cells.SegmentColors>
 -- stylua: ignore
@@ -64,31 +32,19 @@ local colors = {
 local cells = Cells:new()
 
 cells
-    :add_segment('date_icon', ICON_DATE .. '  ', colors.date, attr(attr.intensity('Bold')))
     :add_segment('date_text', '', colors.date, attr(attr.intensity('Bold')))
-    :add_segment('separator', ' ' .. ICON_SEPARATOR .. '  ', colors.separator)
-    :add_segment('battery_icon', '', colors.battery)
+    :add_segment('separator', '   ', colors.separator)
     :add_segment('battery_text', '', colors.battery, attr(attr.intensity('Bold')))
 
----@return string, string
+---@return string
 local function battery_info()
-    -- ref: https://wezfurlong.org/wezterm/config/lua/wezterm/battery_info.html
-
     local charge = ''
-    local icon = ''
 
     for _, b in ipairs(wezterm.battery_info()) do
-        local idx = umath.clamp(umath.round(b.state_of_charge * 10), 1, 10)
         charge = string.format('%.0f%%', b.state_of_charge * 100)
-
-        if b.state == 'Charging' then
-            icon = charging_icons[idx]
-        else
-            icon = discharging_icons[idx]
-        end
     end
 
-    return charge, icon .. ' '
+    return charge
 end
 
 ---@param opts? Event.RightStatusOptions Default: {date_format = '%a %H:%M:%S'}
@@ -100,15 +56,12 @@ M.setup = function(opts)
     end
 
     wezterm.on('update-right-status', function(window, _pane)
-        local battery_text, battery_icon = battery_info()
-
         cells
             :update_segment_text('date_text', wezterm.strftime(valid_opts.date_format))
-            :update_segment_text('battery_icon', battery_icon)
-            :update_segment_text('battery_text', battery_text)
+            :update_segment_text('battery_text', battery_info())
 
         window:set_right_status(
-            wezterm.format(cells:render({ 'date_icon', 'date_text', 'separator', 'battery_icon', 'battery_text' }))
+            wezterm.format(cells:render({ 'date_text', 'separator', 'battery_text' }))
         )
     end)
 end
